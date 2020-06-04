dfdbfef03575f0bb16c15f408b5f2c56c461413a
bkushigian <bkushigian@gmail.com>
2020-06-04 15:05:22 -0700

    Updates

---
+++ tests/BitTwiddle.java
@@ -0,0 +1,10 @@
+public class BitTwiddle {
+  int flp2(int x) {
+    x = x | (x >> 1);
+    x = x | (x >> 2);
+    x = x | (x >> 4);
+    x = x | (x >> 8);
+    x = x | (x >> 16);
+    return x - (x >> 1);
+  }
+}
